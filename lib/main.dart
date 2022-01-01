aa3f682f6a58c0dc504f7e83641056cf868f8d32
Tatsuya Inokuchi <ino1221@gmail.com>
2022-01-01 17:00:25 +0900

    feat: Add a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,11 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
-        body: Center(
-          // child: Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          // child: Text('Hello World3'),
+          // child: Text(wordPair.asPascalCase),
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
